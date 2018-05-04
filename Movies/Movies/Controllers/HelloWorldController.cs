24a315a6a88e7a1c57e9198e916a97b59c4af885
g.gerdjikov <g.gerdjikov@outlook.com>
2018-05-04 14:14:10 +0300

    Created Movies and Students

---
+++ Movies/Movies/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movies.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index() 
+        {
+            //return Hello World!!!;
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 0) 
+        {
+            ViewBag.name = name;
+            ViewBag.numTimes = numTimes;
+            //return String.Format("Welcome {0}!!! {1}", name, numTimes);
+            return View();
+        }
+    }
+}
